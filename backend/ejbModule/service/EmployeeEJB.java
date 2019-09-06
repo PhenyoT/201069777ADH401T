d4c89a9cbc005c594f43485228fd2b4968d54cbe
PhenyoT <PhenyoT@172.16.69.138>
2019-09-07 00:46:09 +0200

    After Commit

--- backend/ejbModule/service/EmployeeEJB.java
+++ backend/ejbModule/service/EmployeeEJB.java
@@ -22,6 +22,17 @@ public class EmployeeEJB {
         // TODO Auto-generated constructor stub
     }
     
+    public List<Employee> findEmployees(){
+    	
+    	List<Employee> data = em.createQuery("SELECT id,dateOfBirth,name,surName FROM employee_tbl",Employee.class).getResultList();
+    	System.out.println(data.get(1).getDateOfBirth());
+    	   System.out.println(data.get(2).getName());
+    	   System.out.println(data.get(3).getSurName());
+    	
+    	return data;
+    }
+
+    
     public void addNew(EmployeeEntity employeeEntity)
     {
     	System.out.println("============================");
